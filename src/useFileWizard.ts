4c9fe544ec57e00c8eb229af3bd4f0a822d2e40b
greenbackboogie97 <greenbackboogie@tutamail.com>
2022-05-28 12:24:04 +0300

    a manager hook, collides cappabilities to manage file uploads from user machine to react app, currently supports audio files

---
+++ src/useFileWizard.ts
@@ -0,0 +1,54 @@
+import { useRef, useState } from 'react'
+import useFileInput from './useFileInput'
+import { decodeAudioFile } from './utils/audioUtils'
+import { Rule, WizardFile } from './types/index'
+
+export interface UseFileWizardProps {
+    type: 'audio' | 'image' | 'video' | 'document'
+    onLoad?: (df: any) => void
+}
+
+export default function useFileWizard(props: UseFileWizardProps) {
+    const { type, onLoad } = props
+    const [loading, setLoading] = useState<boolean>(false)
+    const [file, setFile] = useState<WizardFile>({ readerFile: null })
+    const fileReader = useRef<FileReader>(new FileReader())
+
+    const fileInput = useFileInput({
+        type: type !== 'document' ? type : null,
+        onChange: e => {
+            const readerFile = e.target.files[0]
+
+            fileReader.current.onloadstart = () => setLoading(true)
+            fileReader.current.onload = () => handleReaderLoad(readerFile)
+
+            if (type === 'audio') fileReader.current.readAsArrayBuffer(readerFile)
+        }
+    })
+
+    const handleReaderLoad = (readerFile: File) => {
+        const readerDecode = fileReader.current.result
+
+        if (type === 'audio') {
+            const handleDecodeSuccess: DecodeSuccessCallback = dd => {
+                const file = { readerFile, readerDecode, audioData: dd }
+                setFile(file)
+                onLoad?.(file)
+                setLoading(false)
+            }
+            decodeAudioFile(readerDecode as ArrayBuffer, handleDecodeSuccess)
+        }
+    }
+
+    return {
+        file,
+        loading,
+        click: () => fileInput.current.click(),
+        clear: () => {
+            fileInput.current.value = ''
+            fileReader.current.onload = null
+            setFile({ readerFile: null })
+            setLoading(false)
+        }
+    }
+}
