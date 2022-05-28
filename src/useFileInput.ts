6a9941c84289ae014ede15d89b74779e9ebba414
greenbackboogie97 <greenbackboogie@tutamail.com>
2022-05-28 12:20:17 +0300

    mounts a file input on the DOM with some configs

---
+++ src/useFileInput.ts
@@ -0,0 +1,28 @@
+import { MutableRefObject, useEffect, useRef } from 'react'
+
+export interface UseFileInputProps {
+    type?: 'audio' | 'image' | 'video' | null
+    formats?: string[]
+    onChange: (e: any) => void
+}
+
+export default function useFileInput(props: UseFileInputProps): MutableRefObject<HTMLInputElement> {
+    const { type, formats, onChange: handleChange } = props
+    const fileInput = useRef<HTMLInputElement>(document.createElement('input'))
+
+    useEffect(() => {
+        const currentFileInput = fileInput.current
+
+        currentFileInput.setAttribute('type', 'file')
+
+        if (type && !formats) currentFileInput.setAttribute('accept', `${type}/*`)
+        if (type && formats) currentFileInput.setAttribute('accept', `${type}/*, ${formats.map(f => `.${f}, `)}`)
+        if (!type && formats) currentFileInput.setAttribute('accept', `${formats.map(f => `.${f}, `)}`)
+
+        currentFileInput.addEventListener('change', handleChange)
+
+        return () => currentFileInput.removeEventListener('change', handleChange)
+    }, [handleChange, formats])
+
+    return fileInput
+}
