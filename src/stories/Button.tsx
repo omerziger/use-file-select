86811cf553054a647d49d6125c4d8d31b33e75de
Dean Geva <dean.geva@tikalk.com>
2022-06-13 12:08:56 +0300

    styling touch-ups

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -2,6 +2,24 @@ import React from 'react'
 import useFileWizard from '../useFileWizard'
 import './button.css'
 
+const styles = {
+  progressContainer: {
+    width: '100%',
+    height: '20px',
+    border: '1px solid black',
+    backgroundColor: '#f0f0f0',
+  },
+  progressBar: {
+    backgroundColor: 'green',
+    height: '100%',
+  },
+  progressText: {
+    zIndex: 1,
+    color: 'black',
+    inset: 0,
+  },
+}
+
 export const Button = () => {
   const fileWizard = useFileWizard({ type: 'audio', onLoad: console.log })
   console.log(fileWizard)
@@ -9,29 +27,18 @@ export const Button = () => {
   return (
     <>
       <div onClick={fileWizard.click}>click me</div>
-      <div
-        style={{
-          width: '100%',
-          height: '20px',
-          border: '1px solid black',
-          position: 'relative',
-          backgroundColor: '#f0f0f0',
-        }}
-      >
+      <div style={{ position: 'relative', ...styles.progressContainer }}>
         <div
           style={{
-            backgroundColor: 'green',
-            height: '100%',
+            ...styles.progressBar,
             width: fileWizard.progress + '%',
           }}
         ></div>
         <div
           style={{
-            textAlign: 'center',
-            zIndex: 1,
-            color: 'black',
             position: 'absolute',
-            inset: 0,
+            textAlign: 'center',
+            ...styles.progressText,
           }}
         >
           {fileWizard.progress}%
