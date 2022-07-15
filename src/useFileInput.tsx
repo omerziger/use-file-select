4d103e5492de1104b30ebf417874772237e0cd3d
greenbackboogie97 <greenbackboogie@tutamail.com>
2022-07-15 22:11:37 +0300

    seprated for two useEffects

--- src/useFileInput.tsx
+++ src/useFileInput.tsx
@@ -6,33 +6,23 @@ export interface UseFileInputProps {
   onChange: (e: Event) => void
 }
 
-export default function useFileInput(
-  props: UseFileInputProps
-): MutableRefObject<HTMLInputElement> {
+export default function useFileInput(props: UseFileInputProps): MutableRefObject<HTMLInputElement> {
   const { type, formats, onChange: handleChange } = props
   const fileInput = useRef<HTMLInputElement>(document.createElement('input'))
 
   useEffect(() => {
     const currentFileInput = fileInput.current
-
     currentFileInput.setAttribute('type', 'file')
-
     if (type && !formats) currentFileInput.setAttribute('accept', `${type}/*`)
-    if (type && formats)
-      currentFileInput.setAttribute(
-        'accept',
-        `${type}/*, ${formats.map((f) => `.${f}, `)}`
-      )
-    if (!type && formats)
-      currentFileInput.setAttribute(
-        'accept',
-        `${formats.map((f) => `.${f}, `)}`
-      )
+    if (type && formats) currentFileInput.setAttribute('accept', `${type}/*, ${formats.map((f) => `.${f}, `)}`)
+    if (!type && formats) currentFileInput.setAttribute('accept', `${formats.map((f) => `.${f}, `)}`)
+  }, [formats])
 
+  useEffect(() => {
+    const currentFileInput = fileInput.current
     currentFileInput.addEventListener('change', handleChange)
-
     return () => currentFileInput.removeEventListener('change', handleChange)
-  }, [handleChange, formats])
+  }, [handleChange])
 
   return fileInput
 }
